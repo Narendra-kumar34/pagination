3b4216c52f1a5223e43d92867d9d99196c7fa62a
Narendra <narendrach34@gmail.com>
2024-02-11 23:16:29 +0530

    added alert -

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
       setData(res.data);
     } catch (err) {
       console.log("failed to fetch data", err);
-      alert("failed to fetch data");
+      window.alert("failed to fetch data");
     }
   };
 
